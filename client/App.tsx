38ab51f564c34110def75f2ade7989759c256312
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-10-29 19:00:48 -0300

    jeje primera prueba

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  let [ number, setNumber ] = useState<number>(0);
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    setNumber(number++);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hola flor de gato</Text>
+      <Button
+        onPress={(e) => handleClick(e)}
+        title="GATO MIRA LO QUE HAGO"
+        color="#4827c2"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
+      <Text style={styles.text}>{number}</Text>
     </View>
   );
 }
@@ -17,4 +32,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    marginTop: 30,    
+    fontSize: 30
+  }
 });
