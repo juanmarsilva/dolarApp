aa9d5d255157bc884851298ac0b7a507af3ab5c0
FelipeJure <felipe.jure05@gmail.com>
2022-10-31 23:57:36 +0100

    add some dolars

---
+++ client/App.style.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      width:'100%',
+      height:'100%',
+      backgroundColor: '#333236',
+    },
+    firstDiv: {
+      width: 220,
+      height: 130,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginTop: 80,
+      borderRadius: 12,
+      backgroundColor: '#C2D4E4'
+    },
+    otherDivs: {
+      width: 220,
+      height: 130,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginTop: 40,
+      borderRadius: 12,
+      backgroundColor: '#C2D4E4'
+    },
+  });
+
+export default styles
---
+++ client/Components/Loading.style.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const LoadingStyle = StyleSheet.create({
+    container: {
+        width:'100%',
+        height: '100%',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text : {
+        fontSize: 30,
+        color: '#000'
+    }
+})
+export default LoadingStyle
