f5a969f245de681822ae54fd5fb650cc1d74a078
FelipeJure <felipe.jure05@gmail.com>
2022-11-03 10:43:22 +0100

    changes folders

--- client/Screens/BalanceScreen/BalanceScreen.style.tsx
+++ client/Screens/BalanceScreen/BalanceScreen.style.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { colors } from "../App.style";
+import { colors } from "../../App.style";
 
 const balanceStyle = StyleSheet.create({
     container: {
