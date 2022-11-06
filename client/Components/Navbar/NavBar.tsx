5cb24fded5b2a5cd58d98ffd46a44c0ab84afc73
FelipeJure <felipe.jure05@gmail.com>
2022-11-06 01:52:11 +0100

    add lottie icons

--- client/Components/Navbar/NavBar.tsx
+++ client/Components/Navbar/NavBar.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { View, Pressable } from "react-native";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import { View, Pressable, Text } from "react-native";
 import { styles } from "./NavbarStyles";
 import { colors } from "./NavbarStyles";
+import Pig from "../../Icons/pig";
+import Home from "../../Icons/home";
+import Money from "../../Icons/money";
 
 export const NavBar = ({ navigation, route }: any) => {
+
     return (
         <View style={styles.container}>
         <View style={styles.button}>
             <Pressable onPress={() => navigation.navigate("Balance")}>
-                <Ionicons
-                    name="calculator-outline"
-                    size={40}
-                    color={colors.contrarianText}
-                />
+                <Pig focused={route.name === 'Balance'}/>
+                <Text style={{position:'absolute',left:4, color: route.name === 'Balance'?colors.selected:colors.buttonContainer}}>.</Text>
                 {route.name === "Balance" ? (
                     <View style={styles.balance}></View>
                 ) : (
@@ -23,21 +23,15 @@ export const NavBar = ({ navigation, route }: any) => {
         </View>
         <View style={styles.centralButton}>
             <Pressable onPress={() => navigation.navigate("Home")}>
-                <Ionicons
-                    name="home-outline"
-                    size={60}
-                    color={colors.contrarianText}
-                />
+                <Home focused={route.name === 'Home'}/>
+                <Text style={{position:'absolute', color: route.name === 'Home'?colors.selected:colors.buttonContainer}}>.</Text>
                 {route.name === "Home" ? <View style={styles.home}></View> : <></>}
             </Pressable>
         </View>
         <View style={styles.button}>
             <Pressable onPress={() => navigation.navigate("Profile")}>
-                <Ionicons
-                    name="person-circle-outline"
-                    size={50}
-                    color={colors.contrarianText}
-                />
+                <Money focused={route.name === 'Profile'}/>
+                <Text style={{position:'absolute',left:4, color: route.name === 'Profile'?colors.selected:colors.buttonContainer}}>.</Text>
                 {route.name === "Profile" ? (
                     <View style={styles.profile}></View>
                 ) : (
